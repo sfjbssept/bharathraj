276b117720eb0c0cb33f57e694ec73c7a9fe7081
sfjbssept <sfjbssept@gmail.com>
2022-09-12 06:41:15 +0000

    adding save function

--- ProductsManagementSystems/src/main/java/com/product/service/IProductService.java
+++ ProductsManagementSystems/src/main/java/com/product/service/IProductService.java
@@ -1,5 +1,9 @@
 package com.product.service;
 
+import com.product.entity.Product;
+
 public interface IProductService {
+	
+	public Integer addProduct(Product product); 
 
 }
--- ProductsManagementSystems/src/main/java/com/product/service/ProductServiceImpl.java
+++ ProductsManagementSystems/src/main/java/com/product/service/ProductServiceImpl.java
@@ -1,5 +1,21 @@
 package com.product.service;
 
-public class ProductServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.product.entity.Product;
+import com.product.repository.IProductRepository;
+
+@Service
+public class ProductServiceImpl implements IProductService {
+	
+	@Autowired
+	IProductRepository productRepository;
+
+	@Override
+	public Integer addProduct(Product product) {
+		Product savedProduct = productRepository.save(product);
+		return savedProduct.getId();
+	}
 
 }
