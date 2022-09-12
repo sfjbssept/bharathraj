6ff05a0f60c47a1c0f8219a7daf838aa3c17ec3e
sfjbssept <sfjbssept@gmail.com>
2022-09-12 07:25:27 +0000

    adding get function

--- ProductsManagementSystems/src/main/java/com/product/service/IProductService.java
+++ ProductsManagementSystems/src/main/java/com/product/service/IProductService.java
@@ -1,9 +1,13 @@
 package com.product.service;
 
+import java.util.List;
+
 import com.product.entity.Product;
 
 public interface IProductService {
 	
-	public Integer addProduct(Product product); 
+	public Integer addProduct(Product product);
+
+	public List<Product> getAllProducts(); 
 
 }
--- ProductsManagementSystems/src/main/java/com/product/service/ProductServiceImpl.java
+++ ProductsManagementSystems/src/main/java/com/product/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.product.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,11 @@ public class ProductServiceImpl implements IProductService {
 	public Integer addProduct(Product product) {
 		Product savedProduct = productRepository.save(product);
 		return savedProduct.getId();
+	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
 	}
 
 }
